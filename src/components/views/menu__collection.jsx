00a7a11a67d757b07762c232f1a81b5b3c52388a
AniLiss <anatskayalisa@gmail.com>
2017-02-14 23:30:01 +0300

    toggle menu

--- src/components/views/menu__collection.jsx
+++ src/components/views/menu__collection.jsx
@@ -1,35 +1,22 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import { Link, withRouter } from 'react-router';
 
 class MenuCollection extends Component {
-    toggleMenu = function (e) {
-        console.log('t');
-        console.log(e.target);
-        console.log('ct');
-        console.log(e.currentTarget);
-        let menuCollection = document.querySelector('.menu__collection');
-        for (var i = 0; i < menuCollection.childNodes.length; i++) {
-            menuCollection.childNodes[i].classList.remove('menu__collection-item--active');
-            console.log( menuCollection.childNodes[i]);
+    generateItemClassName(activePath) {
+        if (this.props.location.pathname === activePath) {
+            return 'menu__collection-item menu__collection-item--active';
+        } else {
+            return 'menu__collection-item';
         }
-        e.target.parentNode.classList.add('menu__collection-item--active');
     }
-
-//     menuCollection.addEventListener('click', function (e) {
-//   for (var i = 0; i < menuCollection.childNodes.length; i++) {
-//     menuCollection.childNodes[i].classList.remove('menu__collection-item--active');
-//     console.log( menuCollection.childNodes[i]);
-//   }
-//   e.target.parentNode.classList.add('menu__collection-item--active');
-// });
 
     render() {
         return (
-            <ul className='menu__collection' onClick={this.toggleMenu}>
-                <li className='menu__collection-item menu__collection-item--active'>
+            <ul className='menu__collection'>
+                <li className={this.generateItemClassName('/')}>
                     <Link to='/'>Animation</Link>
                 </li>
-                <li className='menu__collection-item'>
+                <li className={this.generateItemClassName('/easing')}>
                     <Link to='/easing'>Easing</Link>
                 </li>
             </ul>
@@ -37,4 +24,4 @@ class MenuCollection extends Component {
     }
 }
 
-export default MenuCollection;
+export default withRouter(MenuCollection);
